32a142cda22c1324892bf7e559544dabbd437d33
Oleg Kovalevsky <kovalevskiol@mail.ru>
2024-08-28 00:20:57 +0300

    initial front

---
+++ car-shop-app/src/app/_services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  public jsonUrl = 'data.json';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    
+    return this.http.get(this.jsonUrl);
+  }
+}
